40301d879644e7e52429a742875abc2e7446349f
saroz <the.saroz@pm.me>
2021-05-19 20:20:33 +0545

    minor fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,19 +24,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Evoytic`,
+        short_name: `evoytic`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#3744ea`,
+        theme_color: `#3744ea`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/evoytic-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -50,5 +47,6 @@ module.exports = {
         cookieDomain: 'evoytic.com',
       },
     },
+    'gatsby-plugin-offline',
   ],
 }
